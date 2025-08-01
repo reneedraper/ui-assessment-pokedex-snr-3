70ed528d36442a251114f5928fd50a4448152888
Renée Draper <reneee.draper@gmail.com>
2025-08-01 14:43:04 -0600

    (client) Added benchmark logging for sanity check

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,15 +1,19 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  ApolloLink,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
+
+const loggerLink = new ApolloLink((operation, forward) => {
+  const startTime = performance.now();
+  return forward(operation).map((result) => {
+    const timeTaken = performance.now() - startTime;
+    console.log(`[Apollo Benchmark] ${operation.operationName} took ${timeTaken.toFixed(2)} ms`);
+    return result;
+  });
+});
+
+const httpLink = new HttpLink({
+  uri: 'https://graphql-pokeapi.vercel.app/api/graphql',
+});
 
 export const client = new ApolloClient({
-  link: ApolloLink.from([
-    new HttpLink({
-      uri: 'https://graphql-pokeapi.vercel.app/api/graphql',
-    }),
-  ]),
-  cache: new InMemoryCache({}),
+  link: ApolloLink.from([loggerLink, httpLink]),
+  cache: new InMemoryCache(),
 });
