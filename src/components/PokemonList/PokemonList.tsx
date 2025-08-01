36a7326c4b43818c1859fb9179f35aad694c2564
Renée Draper <reneee.draper@gmail.com>
2025-08-01 11:26:32 -0600

    (components) Integrate search bar with list

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,40 +1,61 @@
 import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useGetPokemons } from '../../hooks/useGetPokemons';
 import { PokemonDetailsModal } from '../PokemonModal';
+import { SearchBar } from '../SearchBar';
 
 export const PokemonList = () => {
   const classes = useStyles();
   const { pokemons, loading } = useGetPokemons();
-  const [selectedPokemonName, setSelectedPokemonName] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const { name: selectedPokemonName } = useParams();
 
-  const handleOpen = (name: string) => setSelectedPokemonName(name);
-  const handleClose = () => setSelectedPokemonName(null);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleOpen = (name: string) => {
+    navigate(`/pokemon/${name}`);
+  };
+
+  const handleClose = () => {
+    navigate('/pokemon');
+  };
+
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.trim().toLowerCase())
+  );
+
 
   return (
     <section aria-labelledby="pokemon-heading" className={classes.root}>
-      <h2 id="pokemon-heading" >Pokémon List</h2>
+      <h2 id="pokemon-heading" className={classes.header}>Pokémon List</h2>
+      <SearchBar value={searchTerm} onChange={setSearchTerm} />
       {loading && <div role="status" aria-live="polite">Loading...</div>}
       <ul className={classes.pokemonList}>
-        {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>
-          <button
-            data-testid="pokemon-button"
-            className={classes.pokemonItem}
-            onClick={() => handleOpen(pokemon.name)}
-            aria-haspopup="dialog"
-            aria-controls={`dialog-${pokemon.name}`}
-          >
-            {pokemon.name}
-          </button>
-          <img src={pokemon.image} alt={pokemon.name} width={50} />
-        </li>
-      ))}
-    </ul>
+        {filteredPokemons.map((pokemon) => (
+          <li className={classes.pokemonItem} key={pokemon.id}>
+            <button
+              data-testid="pokemon-button"
+              className={classes.pokemonButton}
+              onClick={() => handleOpen(pokemon.name)}
+              aria-haspopup="dialog"
+              aria-controls={`dialog-${pokemon.name}`}
+            >
+              <img className={classes.pokemonImage} src={pokemon.image} alt={pokemon.name} width={150} />
+              <div className={classes.pokemonItemSummary}>
+                <span>#{pokemon.id}</span>
+                <span className={classes.pokemonName}>{pokemon.name}
+                </span>
+                </div>
+            </button>
 
-    {selectedPokemonName && (
-      <PokemonDetailsModal name={selectedPokemonName} onClose={handleClose} />
-    )}
+          </li>
+        ))}
+      </ul>
+
+      {selectedPokemonName && (
+        <PokemonDetailsModal name={selectedPokemonName} onClose={handleClose} />
+      )}
     </section>
   );
 };
@@ -46,11 +67,46 @@ const useStyles = createUseStyles(
       padding: '32px',
       boxSizing: 'border-box',
     },
-    pokemonList: { listStyleType: 'none' },
-    pokemonItem: { backgroundColor: 'transparent',
-      cursor: 'pointer',
+    header: { color: 'white' },
+    pokemonList: {
+      padding: '0',
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'flex-start',
+      listStyleType: 'none',
     },
+    pokemonItem: {
+      margin: '5px',
+      cursor: 'pointer',
+      transition: 'transform 0.2s ease-in-out',
+      position: 'relative',
+      '&:hover': {
+        transform: 'scale(1.05)',
+      },
+    },
+    pokemonItemSummary: {
+      display: 'flex',
+      gap: '10px',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+    },
+    pokemonImage: {
+      borderRadius: '15px',
+      background: 'rgba(255, 255, 255, .2)',
 
+    },
+    pokemonName: {
+      fontSize: '20px',
+      textTransform: 'capitalize'
+    },
+    pokemonButton: {
+      backgroundColor: 'transparent',
+      color: 'white',
+      cursor: 'pointer',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '5px'
+    },
   },
   { name: 'PokemonList' }
 );
